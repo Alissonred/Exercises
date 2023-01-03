ee24c2ba45698d613ecb05f9a44ad2f410416e3b
Alisson <alissonredondo11042014@gmail.com>
2023-01-02 19:22:57 -0500

    ejercicio 8 pte

--- 07-palindrome/boilerplate/isPalindrome.js
+++ 07-palindrome/boilerplate/isPalindrome.js
@@ -1,5 +1,72 @@
-const isPalindrome = (str) => {
-  // ...
-};
+// const isPalindrome = (str) => {
+//   const realArray = str.split('')
+//   const reverseArray = [...realArray].reverse()
+//   let counter = 0
+//   for(let i =0; i<realArray.length; i++){ realArray[i]=== reverseArray[i] ? counter++ : counter}
+//   if(counter === reverseArray.length){
+//     console.log('it is true');
+//     return true
+//   }else{
+//     console.log('it is false');
+//     return false
+//   }
+// };
 
-module.exports = isPalindrome;
+// isPalindrome('ana')
+// module.exports = isPalindrome;
+///////////////////////////////////////////////////////////////
+// const isPalindrome = (str) => {
+//   let i=0
+//   let j = str.length-1
+//   let victorious = 0
+
+//   for(i , j; i< str.length; i++, j-- ){
+//     //console.log(str[i],'ace', str[j],'decr' );
+// console.log([...str], 'OMG');
+//     if(str[i]=== str[j]){ /// condicion clave 
+//       victorious++
+//     }
+//   }
+//   victorious == str.length ? console.log('true') :console.log('false')
+// };
+
+// isPalindrome('ana')
+//////////////////////////////////////////////////////////////
+// const isPalindrome = str => str === [...str].reverse().join('')
+
+// isPalindrome('anaff')
+
+//////////////////////////////////////////////////////////
+
+// const isPalindrome = str =>{
+//   const middle = Math.floor(str.length/2)
+//   let counter =0
+//   for(let i = 0; i<middle; i++ ){ 
+//     if (str[i]!==str[str.length-1-i]){///// prueba que es diferente, condicion clave
+//       console.log('no palindromo');
+//       return false
+//     }
+//   }
+//   console.log('palindromo');
+//   return true
+ 
+// }
+
+// isPalindrome('stresseddesserts')
+
+const isPalindrome = str =>{
+if(str.length < 2){  //// caso base (mas peq)
+  console.log(true);
+  return true
+}else{
+  if(str[0]!== str[str.length-1]){//// condicion clave 
+    console.log(false);
+    return false
+  }else{
+    console.log('evalua mas peq');
+    isPalindrome(str.slice(1,-1))
+  }
+}
+
+}
+isPalindrome('stresseddesserts')
--- 08-count-vowels/boilerplate/countVowels.js
+++ 08-count-vowels/boilerplate/countVowels.js
@@ -1,5 +1,42 @@
-const countVowels = (str) => {
-  // ...
-};
+// const validateVowels=(letter, ref, counter)=> letter===ref? counter[ref] = counter[ref]+  1 : counter[ref]= counter[ref]
+  
+// const countVowels = (str) => {
+//   let counter={
+//     a : 0,
+//     e : 0,
+//     i : 0,
+//     o : 0,
+//     u : 0
+//   }
+//   for(let i=0; i<str.length; i++){
+//     validateVowels(str[i],'a',counter)
+//     validateVowels(str[i],'e', counter)
+//     validateVowels(str[i],'i',counter)
+//     validateVowels(str[i],'o',counter)
+//     validateVowels(str[i],'u',counter)
+//   }
+//   console.log(counter);
+//   return counter
+// };
 
-module.exports = countVowels;
+// countVowels('aaabrteenkkau')
+
+////////////////////////////////////////////////////////////
+//const validator =(obj, item)=>!obj[item]? obj[item]=1 : obj[item]=obj[item]+1
+const countVowels = (str) => {
+  //console.log([...str].reduce(validator(obj, item), { a: 0, e: 0, i: 0, o: 0, u: 0 }));
+  let objInicial =  { a: 0, e: 0, i: 0, o: 0, u: 0 }
+  let out = [...str].reduce( (counter, item)=> {
+    console.log(counter, 'es counter ', item, 'es item', objInicial[item], 'es objInicial item');
+    //console.log('el retorno es :');
+    //console.log(item == obj[item]);
+    //return !obj[item]? obj[item]=1 : obj[item]=obj[item]+1 }, objInicial);
+    //return objInicial[item]== item ?  objInicial[item] = objInicial[item]+1: objInicial[item] = objInicial[item] , objInicial});
+    
+    objInicial[item]? objInicial[item] = objInicial[item]+1: '' , objInicial}); //// ARREGLAR, VER VIDEO PLATZI
+  console.log(out);
+return out
+
+}
+
+countVowels('aaabrteenkkau')
